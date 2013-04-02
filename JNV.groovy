e4b0f4184e31ffe216b3e2c1fad421a4b716b5a5
vinodkd <kd@vinodkd.org>
2013-04-02 21:38:01 +0530

    code reorg: adds only. git gui complains when the deleted lines are added to this commit

--- JNV.groovy
+++ JNV.groovy
@@ -6,6 +6,117 @@ import java.awt.*
 
 import groovy.json.*
 
+class JNVLauncher{
+    static main(args) {
+        def NOTES = new Notes(System.getProperty("user.dir"))
+        def JNVLauncher launcher = new JNVLauncher(NOTES)
+        launcher.createAndShowUI()        
+    }
+
+    private NOTES
+    private swing
+    private listModel = new DefaultListModel()
+
+    JNVLauncher(notes){
+        NOTES = notes
+    }
+
+    def createAndShowUI(){
+        swing = new SwingBuilder()
+
+        // TODO: add close hook so that unsaved data is saved before close
+        def ui = swing.frame(
+                    title           :'jNV', defaultCloseOperation:JFrame.EXIT_ON_CLOSE, 
+                    pack            :true, 
+                    show            :true,
+                    preferredSize   : [520,600]
+        ) 
+        {
+            vbox {
+                    textField(  id              : 'noteName',
+                                preferredSize   : [500,20],
+                                actionPerformed : { 
+                                    def nName = swing.noteName.getText()
+                                    def searchResult = NOTES.findNotes(nName)
+                                    println(searchResult)
+                                    if(!searchResult){
+                                        NOTES.add(new Note(nName))
+                                        swing.noteContent.requestFocus()
+                                    }
+                                    else{
+                                        searchResult.each { k,v ->
+                                            swing.foundNotes.model.addElement k
+                                        }
+                                    }
+                                }
+                    )
+                    scrollPane(preferredSize: [500,150]){
+                        list(   id                  : 'foundNotes',
+                                layoutOrientation   : JList.VERTICAL,
+                                background          : Color.WHITE,
+                                model               : listModel,
+                                valueChanged        : { lse ->
+                                    def selectedNote = swing.foundNotes.selectedValue
+                                    swing.noteContent.selectAll()
+                                    swing.noteContent.replaceSelection(NOTES.getNote(selectedNote))
+                                }
+                        )
+                    }
+                    scrollPane(
+                        preferredSize:[500,400]    //, 
+                        //horizontalScrollBarPolicy: JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED,
+                        //verticalScrollBarPolicy: JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
+                        ){
+                        // removed size to make it scrollable. Eureka link: http://stackoverflow.com/a/9624094
+                        textArea(id: 'noteContent')
+                        //noteContent.addDocumentListener(new MyDocumentListener())
+                    }
+            }
+        }
+        // TODO: the params here are total hacks. fix this.
+        swing.noteContent.document.addDocumentListener(new AutoSaver(swing, NOTES))
+
+        ui.show()
+    }
+}
+
+// TODO: MAKE THE AUTOSAVE ACTUALLY SANE. RIGHT NOW ALL IT DOES IS SAVE THE WHOLE CONTENT AGAIN AND AGAIN EVERY 20 EVENTS
+class AutoSaver implements DocumentListener {
+
+    private static int COUNT = 0;
+    private static final int SAVE_AT = 20;     // save at 20 events
+    def swing
+    def notes
+    
+    public AutoSaver(swing, notes){
+        this.swing = swing
+        this.notes = notes
+    }
+    public void insertUpdate(DocumentEvent e) {
+        saveIfRequired(e);
+    }
+    public void removeUpdate(DocumentEvent e) {
+        saveIfRequired(e);
+    }
+    public void changedUpdate(DocumentEvent e) {
+        //Plain text components do not fire these events
+    }
+
+    public synchronized saveIfRequired(e){
+        println "saveifeqd: $COUNT"
+        if(COUNT == SAVE_AT){
+            def doc = swing.noteContent.document
+            def text = doc.getText(doc.startPosition.offset, doc.length)
+            notes.setNote(swing.noteName.text, text)
+            notes.save();
+            COUNT = 0
+            println "saved notecontent:$text"
+        }
+        COUNT++;
+    }
+}
+
+
 class Note{
     def title
     def contents
