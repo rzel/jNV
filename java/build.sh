1748f3d900f9b5d68a046b58a0acca2f86e26f7d
vinodkd <kd@vinodkd.org>
2013-04-16 16:04:46 +0530

    Added a "clean all" line to build.sh which should be useful in general.

--- java/build.sh
+++ java/build.sh
@@ -1,5 +1,5 @@
 # build.sh
-if javac -Xlint:deprecation -Xlint:unchecked -d classes -cp classes src/org/stringtree/json/*.java src/org/vinodkd/jnv/*.java; then
+rm -rf classes/*
 if javac -Xlint:deprecation -Xlint:unchecked -d classes -cp classes src/org/vinodkd/jnv/*.java; then
 	jar cvfm bin/jnv.jar MANIFEST.MF -C classes/ . res
 fi
