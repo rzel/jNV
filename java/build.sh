a44d665f7dc297fa18d1fe81f1f21a2e13f253b3
vinodkd <kd@vinodkd.org>
2013-04-17 08:43:41 +0530

    modified build.sh to use a separate manifest file for json and generate a _json.jar. All of this in prep for making java serialization the primary storage mechanism.

--- java/build.sh
+++ java/build.sh
@@ -1,8 +1,9 @@
 # build.sh
 rm -rf classes/*
 
+echo building jnv_json.jar
 if javac -Xlint:deprecation -Xlint:unchecked -d classes -cp classes src/org/stringtree/json/*.java src/org/vinodkd/jnv/*.java; then
-	jar cvfm bin/jnv.jar MANIFEST.MF -C classes/ . res
+	jar cvfm bin/jnv_json.jar MANIFEST_JSON.MF -C classes/ . res
 fi
 
 
