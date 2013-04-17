764a831bdf0d2209f4dbc90ff4ea4e4cd89d3170
vinodkd <kd@vinodkd.org>
2013-04-17 08:42:26 +0530

    modified build.sh to do a "clean" build every time.

--- java/build.sh
+++ java/build.sh
@@ -1,4 +1,6 @@
 # build.sh
+rm -rf classes/*
+
 if javac -Xlint:deprecation -Xlint:unchecked -d classes -cp classes src/org/stringtree/json/*.java src/org/vinodkd/jnv/*.java; then
 	jar cvfm bin/jnv.jar MANIFEST.MF -C classes/ . res
 fi
